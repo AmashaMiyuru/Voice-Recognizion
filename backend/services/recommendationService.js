70b3cece53b604a287315492699f77d313385331
Amasha Miyuru <110710070+AmashaMiyuru@users.noreply.github.com>
2025-03-05 01:33:28 +0530

    Delete backend directory

--- backend/services/recommendationService.js
+++
@@ -1,8 +0,0 @@
-const Service = require('../models/Service');
-
-async function getRecommendedService({ carModel, mileage }) {
-  const services = await Service.find({});
-  return services.filter(service => mileage >= service.recommendedMileage);
-}
-
-module.exports = { getRecommendedService };
